f69a860229921ff6e3e5a7de0cc3279f5e26288e
kyuubi-Yhp <south01village@gmail.com>
2025-05-10 19:15:12 +0300

    add burger

---
+++ script/burger.js
@@ -0,0 +1,17 @@
+// const btnBurger = document.querySelector('.berger__btn')
+// const burgerActive = document.querySelector('.burger__position')
+
+
+// btnBurger.addEventListener('click', () => {
+//   burgerActive.classList.add('burger__position-active')
+//   burgerBtn.classList.toggle('active');
+// });
+
+const burgerBtn = document.querySelector('.berger__btn');
+const burgerMenu = document.querySelector('.burger__position');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  burgerMenu.classList.toggle('burger__position-active');
+});
+
--- script/popit.js
+++ script/popit.js
@@ -1,12 +1,20 @@
 const btnAdd = document.querySelector('.btn__popit-info')
 const block = document.querySelector('.contant__box-info')
 const cross = document.querySelector('.cross__js')
-console.log(cross)
+const overlay = document.querySelector('.overlay')
+
 
 btnAdd.addEventListener('click', () => {
 block.classList.add('contant__box-info-active')
+overlay.classList.add('overlay-active')
 })
 
 cross.addEventListener('click', () => {
   block.classList.remove('contant__box-info-active')
+  overlay.classList.remove('overlay-active')
+})
+
+overlay.addEventListener('click', () => {
+  block.classList.remove('contant__box-info-active')
+  overlay.classList.remove('overlay-active')
 })
