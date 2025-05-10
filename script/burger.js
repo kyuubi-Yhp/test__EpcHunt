c62a03373e1821151ee5c6a5a8a34c35c4bf58a7
kyuubi-Yhp <south01village@gmail.com>
2025-05-10 20:31:20 +0300

    ready

--- script/burger.js
+++ script/burger.js
@@ -11,24 +11,10 @@ burgerBtn.addEventListener('click', () => {
   linkArrow.classList.toggle('link__in-header-burger')
 });
 
-
-
-// const burgerBtn = document.querySelector('.berger__btn');
-// const burgerMenu = document.querySelector('.burger__position');
-// const burgerOverlay = document.querySelector('.burger__overlay');
-
-// burgerBtn.addEventListener('click', () => {
-//   burgerBtn.classList.toggle('active');
-//   burgerMenu.classList.toggle('burger__position-active');
-
-//   burgerOverlay.classList.toggle('burger__overlay-active');
-// });
-
-
 burgerOverlay.addEventListener('click', () => {
-  burgerBtn.classList.remove('active');
-  burgerMenu.classList.remove('burger__position-active');
-  burgerOverlay.classList.remove('burger__overlay-active');
+  burgerBtn.classList.remove('active')
+  burgerMenu.classList.remove('burger__position-active')
+  burgerOverlay.classList.remove('burger__overlay-active')
   linkArrow.classList.toggle('link__in-header-burger')
 });
 
--- script/slider.js
+++ script/slider.js
@@ -1,64 +1,64 @@
-const ACTIVE_SLIDE_CLASSNAME = 'gallery__slide_active';
+const ACTIVE_SLIDE_CLASSNAME = 'gallery__slide_active'
 
-const slidesNodes = Array.from(document.querySelectorAll('.slide'));
-console.log(slidesNodes);
-const prevButtonNode = document.querySelector('.btn__right');
-const nextButtonNode = document.querySelector('.btn__left');
-let activeId;
+const slidesNodes = Array.from(document.querySelectorAll('.slide'))
+console.log(slidesNodes)
+const prevButtonNode = document.querySelector('.btn__right')
+const nextButtonNode = document.querySelector('.btn__left')
+let activeId
 
-init();
+init()
 
 function init() {
     activeId = 0;
     
     prevButtonNode.addEventListener('click', () => {
-        setActiveSlideById(getPrevId());
-    });
+        setActiveSlideById(getPrevId())
+    })
     
     nextButtonNode.addEventListener('click', () => {
-        setActiveSlideById(getNextId());
-    });
+        setActiveSlideById(getNextId())
+    })
 }
 
 function setActiveSlideById(id) {
-    const currentId = activeId;
-    activeId = id;
+    const currentId = activeId
+    activeId = id
 
     slidesNodes[currentId].classList.remove(ACTIVE_SLIDE_CLASSNAME)
     slidesNodes[activeId].classList.add(ACTIVE_SLIDE_CLASSNAME)
 }
 
 function getPrevId() {
-    return activeId === 0 ? slidesNodes.length - 1 : activeId - 1;
+    return activeId === 0 ? slidesNodes.length - 1 : activeId - 1
 }
 
 function getNextId() {
-    return activeId === (slidesNodes.length - 1) ? 0: activeId + 1;
+    return activeId === (slidesNodes.length - 1) ? 0: activeId + 1
 }
 
-let startX = 0;
-let endX = 0;
+let startX = 0
+let endX = 0
 
-const slider = document.querySelector('.list__slider'); // обертка для свайпа
+const slider = document.querySelector('.list__slider')
 
 slider.addEventListener('touchstart', (event) => {
-  startX = event.touches[0].clientX;
-});
+  startX = event.touches[0].clientX
+})
 
 slider.addEventListener('touchend', (event) => {
-  endX = event.changedTouches[0].clientX;
+  endX = event.changedTouches[0].clientX
 
-  handleSwipe();
-});
+  handleSwipe()
+})
 
 function handleSwipe() {
-  const diffX = endX - startX;
+  const diffX = endX - startX
 
-  if (Math.abs(diffX) > 50) { // минимальная длина свайпа в пикселях
+  if (Math.abs(diffX) > 50) { 
     if (diffX > 0) {
-      setActiveSlideById(getPrevId());
+      setActiveSlideById(getPrevId())
     } else {
-      setActiveSlideById(getNextId());
+      setActiveSlideById(getNextId())
     }
   }
 }
