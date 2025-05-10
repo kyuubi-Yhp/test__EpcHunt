6132ea79fcd95bdf53e8f92e87bc832f4a6cd16b
kyuubi-Yhp <south01village@gmail.com>
2025-05-10 20:20:09 +0300

    fix

--- script/burger.js
+++ script/burger.js
@@ -1,31 +1,34 @@
 
+const burgerBtn = document.querySelector('.berger__btn');
+const burgerMenu = document.querySelector('.burger__position')
+const linkArrow = document.querySelector('.link__in-header')
+const burgerOverlay = document.querySelector('.burger__overlay')
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active')
+  burgerMenu.classList.toggle('burger__position-active')
+  linkArrow.classList.toggle('link__in-header-burger')
+  burgerOverlay.classList.toggle('burger__overlay-active')
+});
+
+
+
 // const burgerBtn = document.querySelector('.berger__btn');
 // const burgerMenu = document.querySelector('.burger__position');
-// const linkArrow = document.querySelector('.link__in-header')
+// const burgerOverlay = document.querySelector('.burger__overlay');
 
 // burgerBtn.addEventListener('click', () => {
 //   burgerBtn.classList.toggle('active');
 //   burgerMenu.classList.toggle('burger__position-active');
-//   linkArrow.classList.toggle('link__in-header-burger')
+
+//   burgerOverlay.classList.toggle('burger__overlay-active');
 // });
-
-
-const burgerBtn = document.querySelector('.berger__btn');
-const burgerMenu = document.querySelector('.burger__position');
-const burgerOverlay = document.querySelector('.burger__overlay');
-
-burgerBtn.addEventListener('click', () => {
-  burgerBtn.classList.toggle('active');
-  burgerMenu.classList.toggle('burger__position-active');
-
-  burgerOverlay.classList.toggle('burger__overlay-active');
-});
 
 
 burgerOverlay.addEventListener('click', () => {
   burgerBtn.classList.remove('active');
   burgerMenu.classList.remove('burger__position-active');
-  
+  linkArrow.classList.toggle('link__in-header-burger')
   burgerOverlay.classList.remove('burger__overlay-active');
 });
 
